bc426cf183aeaf6cd3b5cefeb4ec6b66290a2e3b
Hemanth Kumar Y S <youngstarhemanth@gmail.com>
2018-03-07 18:09:59 +0530

    Written new program in Qtn34.py

--- Programs/Qtn34.py
+++ Programs/Qtn34.py
@@ -1,39 +1,11 @@
-class __SimpleError(Exception):
-  pass
+class operatorNum:
 
-try:
-  raise __SimpleError('Not found')
-except __SimpleError as error:
-  print (error)
-except Exception as error:
-  print ("Error :",error)
-
-
-class sample:
-  
-  def runMe(self):
-    print ("It's runMe() method!!, and this class called this method: ", self.__class__.__name__)
-
-  def whatsup(self):
-    raise NotImplementedError("You're calling not implemented methods!!")
-    
-class simple(sample)  :
-  def name(self):
-    print ("Class Name :",self.__class__.__name__)
-    
-  def whatsup(self):
-    print ("MRO :", self.__class__.__mro__)
-    print ("What's Up!!")
-    
-try:
-  hi = simple()
-  so = sample()
-  so.runMe()
-  #so.whatsup()
-  hi.runMe()
-  hi.name()
-  #print (simple.__mro__)
-  hi.whatsup()
-  
-except NotImplementedError as error:
-  print (error)
+	'This class is the sample  class to check somthing'
+	def __init__(self):
+		self.name = ''
+		
+	def displayAttributes(self):
+		print ("This is the complete dict of current instance : {}".format(self.__dict__))
+		
+run = operatorNum()
+run.displayAttributes()
