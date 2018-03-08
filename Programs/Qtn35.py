7d2b8cc2a763243969578d5a7610ab46ff3ace4b
Hemanth Kumar Y S <youngstarhemanth@gmail.com>
2018-03-08 14:21:32 +0530

    Written something inside a program

--- Programs/Qtn35.py
+++ Programs/Qtn35.py
@@ -1,4 +1,17 @@
 class Queue:
-
-	def __init__(self):
+	mobList = []
+	def __init__(self, mobile, price):
+		self.mobile = mobile
+		self.price = price
+		Queue.mobList.append('Hi')
 		
+	def totalMobiles(self):
+		print (Queue.mobList)
+		print (hex(id(self)))
+		
+mob1 = Queue('Redmi 5A', 10000)
+mob1.totalMobiles()
+#print (mob1.__dir__())
+print (__builtins__.__dir__())
+
+#print (mob1.__class__.__dict__)
