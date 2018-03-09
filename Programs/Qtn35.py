1f1fe62f820edfdd3797ec8c636519f8e185d174
Hemanth Kumar Y S <youngstarhemanth@gmail.com>
2018-03-09 11:44:20 +0530

    Update Qtn.py program

--- Programs/Qtn35.py
+++ Programs/Qtn35.py
@@ -3,15 +3,20 @@ class Queue:
 	def __init__(self, mobile, price):
 		self.mobile = mobile
 		self.price = price
-		Queue.mobList.append('Hi')
+		Queue.mobList.append(self.__dict__)
 		
 	def totalMobiles(self):
 		print (Queue.mobList)
-		print (hex(id(self)))
+	
+	def process(self):
+		Queue.mobList.remove(self.__dict__)
 		
 mob1 = Queue('Redmi 5A', 10000)
 mob1.totalMobiles()
-#print (mob1.__dir__())
-print (__builtins__.__dir__())
-
-#print (mob1.__class__.__dict__)
+mob2 = Queue('Samsung Galaxy', 12000)
+mob3 = Queue('Apple X', 100000)
+Queue.totalMobiles(mob1)
+mob1.price = 9999
+mob1.totalMobiles()
+mob1.process()
+mob1.totalMobiles()
