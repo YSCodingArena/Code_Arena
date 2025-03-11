4060bdb28d9bb2118bea3ff7b1cd12d7c7d11e2d
Hemanth Kumar Y S <youngstarhemanth@gmail.com>
2025-03-11 10:51:28 +0530

    Simple Changes

--- Programs/Class - 1.py
+++ Programs/Class - 1.py
@@ -1,4 +1,4 @@
-#===============================================================================
+# ===============================================================================
 # class Student:
 #     ''' Data of student'''
 #     def __init__(self, first, last, score):
@@ -6,49 +6,55 @@
 #         self.last = last
 #         self.score = score
 #         self.email = first.lower() + "." + "".join(last.split(" ")).lower() + "@gmail.com"
-#         
+#
 #     @property
 #     def fullname(self):
 #         return '{} {}'.format(self.first, self.last)
-#         
-# 
+#
+#
 # std_1 = Student('Hemanth', 'Kumar Y S', 510)
 # #std_2 = Student()
-# 
+#
 # #std_1.first = "Hemanth"
 # #std_1.last = "Kumar Y S"
 # #std_1.email = "hemanth.kumarys@gmail.com"
-# 
+#
 # #print (std_1.first, std_1.last)
 # #print (std_1.__dict__)
 # print ('score = {}, email = {}'.format(std_1.score, std_1.email))
 # print (std_1.fullname())
 # print (Student.fullname(std_1))
-#===============================================================================
-
+# ===============================================================================
+import sys
+import json
+import time
+import os
 
 
 class calculation:
+    """
+    _summary_
+
+    """
     var1 = 1
-    
-    
-    def __init__(self,a,b):
+
+    def __init__(self, a, b):
         self.a = a
         self.b = b
-        
-    def increVar(self):
-        calculation.var1 += 5        
-        
-    def addNum(self):
-        self.increVar()
-        print (self.a + self.b)
-    
-    def subNum(self):
-        self.increVar()
-        print (calculation.var1 - self.a)
-    
 
-calc = calculation(10,20)
-calc1 = calculation(30,40)
+    def incrVar(self):
+        calculation.var1 += 5
+
+    def addNum(self):
+        self.incrVar()
+        print(self.a + self.b)
+
+    def subNum(self):
+        self.incrVar()
+        print(calculation.var1 - self.a)
+
+
+calc = calculation(10, 20)
+calc1 = calculation(30, 40)
 calc.addNum()
 calc1.subNum()
--- Programs/Qtn35.py
+++ Programs/Qtn35.py
@@ -1,20 +1,25 @@
+import requests
+import paramiko
+
 class Queue:
-	mobList = []
-	def __init__(self, mobile, price):
-		self.mobile = mobile
-		self.price = price
-		Queue.mobList.append(self.__dict__)
-		
-	def totalMobiles(self):
-		print (Queue.mobList)
-	
-	def process(self):
-		Queue.mobList.remove(self.__dict__)
-		
-mob1 = Queue('Redmi 5A', 10000)
+    mobList = []
+
+    def __init__(self, mobile, price):
+        self.mobile = mobile
+        self.price = price
+        Queue.mobList.append(self.__dict__)
+
+    def totalMobiles(self):
+        print(Queue.mobList)
+
+    def process(self):
+        Queue.mobList.remove(self.__dict__)
+
+
+mob1 = Queue("Redmi 5A", 10000)
 mob1.totalMobiles()
-mob2 = Queue('Samsung Galaxy', 12000)
-mob3 = Queue('Apple X', 100000)
+mob2 = Queue("Samsung Galaxy", 12000)
+mob3 = Queue("Apple X", 100000)
 Queue.totalMobiles(mob1)
 mob1.price = 9999
 mob1.totalMobiles()
